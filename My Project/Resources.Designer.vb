13b5ea3d6c619a952bf94462252b53b40e9cde3e
Exchord <xpchord@gmail.com>
2023-01-04 23:31:28 +0100

    Move files

--- My Project/Resources.Designer.vb
+++ My Project/Resources.Designer.vb
@@ -4663,16 +4663,6 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
-        Friend ReadOnly Property background() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("background", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
         Friend ReadOnly Property cursor() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("cursor", resourceCulture)
